77bfd750c93eea65c9b85e0782e41dfe742bc12e
Lucien Morey <lucienp@live.com.au>
2021-05-04 10:48:14 +1000

    laid out pseudocode for rmrc traj segment

--- matlab/@TrajectoryGenerator/TrajectoryGenerator.m
+++ matlab/@TrajectoryGenerator/TrajectoryGenerator.m
@@ -11,7 +11,9 @@ classdef TrajectoryGenerator < handle
     end
 
     properties(Constant)
-        minManipMeasure = 0.1;
+        EPSILON = 0.1;
+        VELOCITY_WEIGHTING = diag([1,1,1,0.1,0.1,0.1]);
+        STEPS = 200;
     end
     
     methods
@@ -34,7 +36,7 @@ classdef TrajectoryGenerator < handle
             obj.reloadLocation = reloadLocation;
         end
         
-        function [qMatrix,vMatrix] = GenerateTrajectory(obj, velocityVector)
+        function [qMatrix,vMatrix, tMatrix] = GenerateTrajectory(obj, velocityVector)
             % determine velocity magnitude
             velocityMagnitude = sqrt(sum(velocityVector.^2));
 
@@ -73,7 +75,59 @@ classdef TrajectoryGenerator < handle
 
         end
 
-        
+        function [qMatrix, vMatrix, tMatrix] = GenerateRMRCSegment(obj, startPoint, endPoint, startTime, desiredVelocity, segmentTime)
+            % break up traj segment into cartesian point array of size steps
+
+            % get transform of first point
+            
+            % estimation for initial joint state
+
+            % initial joint state
+
+            % create joint state traj
+            for i=1:1:obj.STEPS
+                % determine forward kinematics solution of current joint states
+
+                % determine the position delta to the next traj point
+
+                % determine linear velocity for this timestep
+
+                % determine current rotation matrix
+
+                % determine future rotation matrix
+
+                % determine rotation error
+
+                % skew the matrix and determine the angular velocity
+
+                % concatinate velocity vectors and scale
+
+                % determine the current jacobian
+
+                % determine the current measure of manipulibility
+
+                % check if a damped least squares solution is required
+                %if m < obj.EPSILON
+                    %least squares
+                %else
+                    %not required
+                %end
+                
+                % apply least squares if required and invert jacobian
+
+                % determine joint velocities
+
+                % check if expected to exceed joint limits
+
+                % update next joint state in qMatrix
+                % update next velocity state in vMatrix
+                % update next times step in tMatrix 
+
+
+            end
+
+        end
+
         function timeDelta = CalculateTimeDelta(obj, p1, p2, desiredVelocity)
             timeDelta = sqrt(sum((((p2-p1)./desiredVelocity).^2)));
         end
