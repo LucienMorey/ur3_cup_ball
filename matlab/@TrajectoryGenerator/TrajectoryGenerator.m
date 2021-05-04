f4a78976d69aeead8df61e165c3f517dfdd339e1
Lucien Morey <lucienp@live.com.au>
2021-05-04 12:14:21 +1000

    filled in RMRC segment generation

--- matlab/@TrajectoryGenerator/TrajectoryGenerator.m
+++ matlab/@TrajectoryGenerator/TrajectoryGenerator.m
@@ -38,7 +38,7 @@ classdef TrajectoryGenerator < handle
         
         function [qMatrix,vMatrix, tMatrix] = GenerateTrajectory(obj, velocityVector)
             % determine velocity magnitude
-            velocityMagnitude = sqrt(sum(velocityVector.^2));
+            velocityMagnitude = sqrt(sum(sum(velocityVector.^2)));
 
             % project along velocity vector to find start and stop locations of throw
             velocityDirection = velocityVector./velocityMagnitude;
@@ -91,43 +91,61 @@ classdef TrajectoryGenerator < handle
             % create joint state traj
             for i=1:1:obj.STEPS-1
                 % determine forward kinematics solution of current joint states
+                currentT = robot.fkine(qMatrix(i,:));
 
                 % determine the position delta to the next traj point
+                deltaX = x(:,i+1) - T(1:3,4);
 
                 % determine linear velocity for this timestep
+                linearVelocity = (1/deltaT)*deltaX;
 
                 % determine current rotation matrix
+                currentR = currentT(1:3,1:3);
 
                 % determine future rotation matrix
+                nextR = rpy2r=(theta(1,i+1),theta(2,i+1),theta(3,i+1))
 
-                % determine rotation error
+                % determine rotation change over time
+                deltaR = (1/deltaT)*(nextR - currentR);
 
                 % skew the matrix and determine the angular velocity
-
+                S = deltaR*currentR'
+                angularVelocity = [S(3,2);S(1,3);S(2,1)];
                 % concatinate velocity vectors and scale
+                cartesianVelocity = obj.VELOCITY_WEIGHTING * [linearVelocity; angularVelocity];
 
                 % determine the current jacobian
+                J = robot.jacob0(qMatrix(i,:));
 
                 % determine the current measure of manipulibility
+                m = sqrt(det(J*J'));
 
                 % check if a damped least squares solution is required
-                %if m < obj.EPSILON
-                    %least squares
-                %else
-                    %not required
-                %end
+                if m < obj.EPSILON
+                    % least squares
+                    lambda = (1 - m/obj.EPSILON)*5E-2;
+                else
+                    % not required
+                    lambda = 0;
+                end
                 
                 % apply least squares if required and invert jacobian
+                invJ = inv(J'*J + lambda *eye(6))*J';
 
                 % determine joint velocities
+                vMatrix(i,:) = invJ*cartesianVelocity
 
                 % check if expected to exceed joint limits
+                for j=1:1:robot.n % Loop through joints 1 to 6
+                    if qMatrix(i,j) + deltaT*vMatrix(i,j) < robot.qlim(j,1) % If next joint angle is lower than joint limit...
+                        vMatrix(i,j) = 0;  % Stop the motor
+                    elseif qMatrix(i,j) + deltaT*qdot(i,j) > p560.qlim(j,2) % If next joint angle is greater than joint limit ...
+                        vMatrix(i,j) = 0; % Stop the motor
+                    end
+                end
 
                 % update next joint state in qMatrix
-                % update next velocity state in vMatrix
-                % update next times step in tMatrix 
-
-
+                qMatrix(i+1,:) = qMatrix(i,:) + deltaT*vMatrix(i,:);
             end
 
         end
