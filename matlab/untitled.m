e27edf27cf54381e51ef47fa63df5eab72d564f2
Lucien Morey <lucienp@live.com.au>
2021-05-04 13:15:49 +1000

    updated test script to new format

--- matlab/untitled.m
+++ matlab/untitled.m
@@ -1,8 +1,8 @@
 clc; close all; clear;
 robot = UR5(false);
-traj = TrajectoryGenerator(robot.model, [0.2,0,0], 0.3,0.1,[0,0,0]);
+traj = TrajectoryGenerator(robot.model, transl(0.2,0,0), 0.3,0.1, transl(0,0,0));
 
-[qMatrix,vMatrix,tMatrix] = traj.GenerateTrajectory([0,0,1]);
+[qMatrix,vMatrix,tMatrix] = traj.GenerateTrajectory([0;0;1]);
 
 figure()
 robot.model.plot(qMatrix,'trail','r-')
