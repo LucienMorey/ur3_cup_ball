c8be2ff0c8bde774e3690fe4547cbd7c5c9e07fe
jm <jmcnamara256@gmail.com>
2021-05-07 11:27:47 +1000

    Added subplots, started ros

--- projectile/gui.m
+++ projectile/gui.m
@@ -9,7 +9,10 @@ close all;
 %  -------------------------
 
 % the pushbotton should set this true, when its time to FIRE
-go = false;
+go      = false;
+robot_q = zeros(1, 6);
+start   = zeros(1, 3);
+goal    = zeros(1, 3);
 
 
 %% Create a maximised figure
@@ -17,8 +20,18 @@ fig = figure('units','normalized','outerposition',[0 0 1 1]);
 
 %% --------SUBPLOT 1--------
 %  -------------------------
+
+%{
+    This plot shows the UR3 in 3d. 
+    In the main loop, the joint states are recieved and the ur3 position
+    should be updated.
+
+    When the go button is pressed, calculate the trajectory then draw it in
+    3D on the plot so we can see where the ball will go.
+%}
+
 % UR3 subplot
-subplot(1, 2, 1);
+h_plot1 = subplot(1, 2, 1);
 
 % create ur3
 ur3 = UR3m(trotz(pi/2));
@@ -27,7 +40,35 @@ ur3 = UR3m(trotz(pi/2));
 view(30, 20);
 daspect([1 1 1]);
 
+%% --------SUBPLOT 2--------
+%  -------------------------
+
+%{
+    This subplot just displays the 2D trajectory
+%}
+
+h_plot2 = subplot(1, 2, 2);
+grid on;
+title('Trajectory 2D x,y');
+xlabel('X (m)');
+ylabel('Y (m)');
+daspect([1 1 1]);
+set(gcf,'color','w');
+xlim([0 1.2]);
+ylim([0 1.2])
+pos = get(gca, 'OuterPosition');
+set(gca, 'OuterPosition', pos);
+
+
 %% Create goal indicators for x, y, z
+
+%{
+    These are just output text so you can see the current X,Y,Z of the cup.
+    The labels dont need to change, but each loop should update the goal
+    fields. This can be done using the line I commented out below, for each
+    x, y, z.
+%}
+
 % these values should update as the cup pose changes
 % TODO, subscribe to cup pose and convert to XYZ
 xlabel = uicontrol('Style', 'text', 'String', 'x-goal', 'position', [20 80 100 15]);
@@ -39,22 +80,69 @@ ygoal = uicontrol('Style', 'text','String', num2str(0), 'position', [150 40 100 
 zlabel = uicontrol('Style', 'text','String', 'z-goal', 'position', [280 80 100 15]);
 zgoal = uicontrol('Style', 'text','String', num2str(0), 'position', [280 40 100 30]);
 
-% use this to update the values in the fields
 % set(xgoal, 'String', num2str(n));
 
+
 %% Create GO button
+
+%{
+    Create the go button. When this is pressed -> calculate trajectory ->
+    display it on figure -> publish go message
+%}
 gobutton = uicontrol('String', 'Launch!', 'position', [150 120 100 40]);
 gobutton.Callback = @gocb;
 
+
 %% --------MAIN GUI LOOP--------
 %  -----------------------------
-while go == false
-    pause(0.1);
-    disp('nup');
-end
-disp('go became true');
 
-%% Functions
+% ROS master address
+rosinit();
+
+% subscriber
+% joint states for disp
+roboqsub = rossubscriber('/joint_states');
+
+% transform tree
+tree = rostf;
+
+%{
+    Main GUI loop, update figures, get msg from subscriber, get tf from
+    tree.
+
+    If go message get publish, calcualte trajectory, output 3d on plot1
+    (with ur3)
+    and 2d on plot2.
+
+%}
+while true
+    
+    % get joint states
+    msg = recieve(roboqsub, 1);
+    q_robo = msg.Position;
+    ur3.model.animate(q_robo);
+    
+    
+    % dunno if necessary, but since we animate here maybe slow it down
+    pause(0.01);
+end
+
+% finish ROS
+rosshutdown;
+
+
+
+%% Callbacks
+
+%{
+    Callback for go button.
+
+    Currently sets the go variable, use this to check if its time to
+    calculate trajectory.
+
+    TODO: THIS CALLBACK SHOULD PUBLISH THE GO MESSAGE SO THAT THE ROBOT
+    KNOWS TO MOVE
+%}
 function gocb(src, event)
     
     % button press
@@ -62,5 +150,7 @@ function gocb(src, event)
     
     % send this value to the base workspace variable 'go'
     assignin('base', 'go', g);
+    
+    % PUBLISH MESSAGE GOGOGO
 end
 
