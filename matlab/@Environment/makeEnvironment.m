d332575348415215fec52ae53d52fa106ef923b0
jmcnamara256 <jmcnamara256@gmail.com>
2021-05-16 23:45:49 +1000

    Removed unneeded brackets

--- matlab/@Environment/makeEnvironment.m
+++ matlab/@Environment/makeEnvironment.m
@@ -1,2 +1,2 @@
-cup = [Environment((transl(0,0,0)*rpy2tr(0,0,0)),'cup.PLY')]
-metal_base = [Environment((transl(0,0,0)*rpy2tr(0,0,0)),'metal table thing.PLY')]
+cup = Environment((transl(0,0,0)*rpy2tr(0,0,0)),'cup.PLY');
+metal_base = Environment((transl(0,0,0)*rpy2tr(0,0,0)),'metal table thing.PLY');
