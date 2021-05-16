846ccf2b6701d90ba9022e73375c1dba0ca509f2
jmcnamara256 <jmcnamara256@gmail.com>
2021-05-16 21:35:54 +1000

    ur3 plots model instead of sticks

--- matlab/@UR3m/UR3m.m
+++ matlab/@UR3m/UR3m.m
@@ -18,6 +18,7 @@ classdef UR3m < handle
 
             % define DH parameters and create serial link
             obj.GetRobot();
+            obj.PlotAndColourRobot();
 
             % if a base was given, move the robo
             if nargin > 0
@@ -26,7 +27,7 @@ classdef UR3m < handle
 
             % Create plot with robot, get colours from ply file
             q0 = ones(1, 6);
-            obj.model.plot(q0, 'workspace', obj.workspace);
+            %obj.model.plot(q0, 'workspace', obj.workspace);
 
         end
         
@@ -57,5 +58,44 @@ classdef UR3m < handle
             % Create the robot with serial link
             obj.model = SerialLink(L);
         end
+
+        %% PlotAndColourRobot
+        % Given a robot index, add the glyphs (vertices and faces) and
+        % colour them in if data is available 
+        function PlotAndColourRobot(obj)
+            % Load Ply files
+            % for each link...
+            for linkIndex = 0:obj.model.n
+                % read play file
+                [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['@UR3m/ply2/l',num2str(linkIndex),'.ply'],'tri');
+                
+                % add to model
+                obj.model.faces{linkIndex+1} = faceData;
+                obj.model.points{linkIndex+1} = vertexData;
+            end
+
+            % Display robot (plot3d)
+            obj.model.plot3d(zeros(1,obj.model.n),'noarrow','workspace',obj.workspace);
+            if isempty(findobj(get(gca,'Children'),'Type','Light'))
+                camlight
+            end  
+            obj.model.delay = 0;
+
+            % Try to correctly colour the arm (if colours are in ply file data)
+            % for each link...
+            for linkIndex = 0:obj.model.n
+                handles = findobj('Tag', obj.model.name);
+                h = get(handles,'UserData');
+                try 
+                    h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+                                                                  , plyData{linkIndex+1}.vertex.green ...
+                                                                  , plyData{linkIndex+1}.vertex.blue]/255;
+                    h.link(linkIndex+1).Children.FaceColor = 'interp';
+                catch ME_1
+                    disp(ME_1);
+                    continue;
+                end
+            end
+        end  
     end
 end
